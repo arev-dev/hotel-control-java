130da90ad6f1afef69053d4898bfce0f0d6e2278
kathedev <katherinenonasco@gmail.com>
2024-06-12 22:23:49 -0600

    feat: creacion de clases bl

---
+++ src/main/java/BL/PayBL.java
@@ -0,0 +1,28 @@
+package BL;
+
+import EN.Pay;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class PayBL {
+    public int createPay(Pay Pay) throws SQLException{
+        return PayDAL.createPay(Pay);
+    }
+
+    public int updatePay(Pay Pay) throws SQLException{
+        return PayDAL.updatePay(Pay);
+    }
+
+    public int deletePay(Pay Pay) throws SQLException{
+        return PayDAL.deletePay(Pay);
+    }
+
+    public ArrayList<Pay> getAll() throws SQLException{
+        return PayDAL.getAll();
+    }
+
+    public ArrayList<Pay> getFilterData(Pay Pay) throws SQLException{
+        return PayDAL.getFilterData(Pay);
+    }
+}
---
+++ src/main/java/BL/ReservationBL.java
@@ -0,0 +1,29 @@
+package BL;
+
+import EN.Reservation;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ReservationBL {
+    public int createReservation(Reservation Reservation) throws SQLException{
+        return ReservationDAL.createReservation(Reservation);
+    }
+
+    public int updateReservation(Reservation Reservation) throws SQLException{
+        return ReservationDAL.updateReservation(Reservation);
+    }
+
+    public int deleteReservation(Reservation Reservation) throws SQLException{
+        return ReservationDAL.deleteReservation(Reservation);
+    }
+
+    public ArrayList<Reservation> getAll() throws SQLException{
+        return ReservationDAL.getAll();
+    }
+
+    public ArrayList<Reservation> getFilterData(Reservation Reservation) throws SQLException{
+        return ReservationDAL.getFilterData(Reservation);
+    }
+}
