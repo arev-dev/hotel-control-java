5790ae4e4b48e193c4ea76c38fdd977bd1e0f30a
Daniel Rodriguez <arevdevgames@gmail.com>
2024-06-12 22:34:46 -0600

    yuda

--- src/main/java/UI/HotelControl.java
+++ src/main/java/UI/HotelControl.java
@@ -1,45 +1,94 @@
 package UI;
 
+import BL.ClientBL;
+import EN.Client;
+
 import javax.swing.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class HotelControl extends JFrame{
     private JPanel MainPanel;
-    private JButton crearNuevoButton;
-    private JButton editarButton;
-    private JButton clientesButton;
-    private JButton habitacionesButton;
-    private JButton pagosButton;
-    private JButton reservacionesButton;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JTextField textField6;
-    private JComboBox comboBox1;
-    private JButton limpiarCamposButton;
+    private JButton BtnCreate;
+    private JButton BtnEdit;
+    private JButton BtnClients;
+    private JButton BtnRooms;
+    private JButton BtnPays;
+    private JButton BtnReservations;
+    private JTextField txt1;
+    private JTextField txt2;
+    private JTextField txt3;
+    private JTextField txt4;
+    private JTextField txt5;
+    private JTextField txt6;
+    private JComboBox CbSearchBy;
+    private JButton BtnClean;
     private JTable table1;
+    private JPanel TitlePanel;
+    private JPanel ToolsPanel;
+    private JPanel BodyPanel;
+    private JPanel FieldsPanel;
+    private JPanel TabsPanel;
+    private JScrollPane TablePanel;
+    private JLabel lbl1;
+    private JLabel lbl2;
+    private JLabel lbl3;
+    private JLabel lbl4;
+    private JLabel lbl5;
+    private JLabel lbl6;
+    private JButton BtnDelete;
+    private JButton BtnSearch;
+    private JButton BtnExit;
 
-    public static void main(String[] args) {
+
+    // instancias propias (creadas por nosotros)
+    ArrayList<Client> clientList;
+    Client client;
+    ClientBL estudianteBL = new ClientBL();
+
+    public static void main(String[] args) throws SQLException {
         new HotelControl();
     }
 
 
-    public HotelControl()
-    {
+    public HotelControl() throws SQLException {
         inicializar();
+        updateForm();
     }
 
     void inicializar(){
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(900, 700);
-        setTitle("Control de Estudiantes");
+        setTitle("Control de Hotel");
         setLayout(null);
         setLocationRelativeTo(null);
         setResizable(false);
 
         setContentPane(MainPanel);
         setVisible(true);
+    }
+
+    void updateForm() throws SQLException {
+        txt1.setText("");
+        txt2.setText("");
+        txt3.setText("");
+        txt4.setText("");
+        txt5.setText("");
+        txt6.setText("");
+        CbSearchBy.setSelectedItem(0);
+
+        txt1.setEnabled(false);
+        txt2.setEnabled(false);
+        txt3.setEnabled(false);
+        txt4.setEnabled(false);
+        txt5.setEnabled(false);
+
+
+        BtnCreate.setEnabled(false);
+        BtnEdit.setEnabled(false);
+        BtnDelete.setEnabled(false);
+
+       // llenarTabla(estudianteBL.obtenerTodos());
     }
 
 }
